de4a1f5a3cb0fd7907061d80bb4157490045c72e
toey02354 <nattapoln02354@gmail.com>
2020-10-21 15:22:01 +0700

    3

--- homeScript.js
+++ homeScript.js
@@ -1,7 +1,16 @@
-let video = document.querySelector(".video");
-let fadeDivider = 1000;
+let video = document.querySelector(".column1");
+let fadeDivider = 900;
 window.addEventListener('scroll', () => {
     let scrollvalue = window.scrollY;
     opacityValue = 1 - scrollvalue/fadeDivider;
     video.style.opacity = opacityValue;
+})
+
+let fadeDivider2 = 600;
+let pictures = document.querySelector(".picture1");
+window.addEventListener('scroll', () => {
+    let scrollValue2 = window.scrollY;
+    console.log(scrollValue2);
+    opacityValue2 = scrollValue2/fadeDivider;
+    pictures.style.opacity = opacityValue2;
 })

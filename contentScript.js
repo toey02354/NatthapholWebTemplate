557bf7a9e03f0cdac44451415f15ff541fee9900
toey02354 <nattapoln02354@gmail.com>
2020-10-21 17:53:34 +0700

    scroll fade contents\

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,12 @@
 let forum2 = document.querySelector(".forum2");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+let forum3 = document.querySelector(".forum3");
+window.addEventListener('scroll', ()=>{
+  let scrollValue = window.scrollY;
+  console.log(scrollValue);
+  if (scrollValue >= 800) {
+    forum2.style.opacity = 1;
   }
-}
+  if (scrollValue >= 1800) {
+    forum3.style.opacity = 1;
+  }
+})

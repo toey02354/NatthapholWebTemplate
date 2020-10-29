784f32e2d84d678194e8357f175e1d9a3fd40fa3
toey02354 <nattapoln02354@gmail.com>
2020-10-30 00:22:20 +0700

    a

--- contactScript.js
+++ contactScript.js
@@ -1,13 +1,10 @@
-import React from "react";
-
-// let button1 = document.getElementsByClassName("buttonA");
+let button1 = document.getElementsByClassName("buttonA");
 let myName = document.getElementById('getName').value
 
 function test1() {
     window.alert("Hello by windows.alert");
 }
 function test2(){
-    const reactVersion = React.version;
     console.log(reactVersion);
 }
 function name1() {
